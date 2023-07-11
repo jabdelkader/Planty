1da0bae88425a0f848143ee00184ea7dd94e3ce6
jabdelkader <abdelkader.jihane@gmail.com>
2023-07-11 15:31:48 +0200

    version3

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -5,3 +5,4 @@ function theme_enqueue_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css'));
 }
+
