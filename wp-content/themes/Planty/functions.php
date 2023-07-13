8cc79b75ae831c98a202bb3a1836006c1b8057df
jabdelkader <abdelkader.jihane@gmail.com>
2023-07-13 16:10:04 +0200

    version 4 il manque le hook

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -6,3 +6,14 @@ function theme_enqueue_styles()
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css'));
 }
 
+
+
+function pd_logout_menu_link( $menu_items, $args ) {
+   if ($args->theme_location == 'Desktop Horizontal Menu') {
+      if (is_user_logged_in()) {
+         $menu_items .= '<li><a href="'. get_admin_url() .'">Admin</a></li>';
+      }
+   }
+   return $menu_items;
+}
+add_filter( 'wp_nav_menu_items', 'pd_logout_menu_link', 10, 2 );
