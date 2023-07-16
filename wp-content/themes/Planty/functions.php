860d1c8ce0c911aefa190d86a376a3b1f85fed30
jabdelkader <abdelkader.jihane@gmail.com>
2023-07-16 20:10:39 +0200

    version avant final (manque de personnalisation du lien admin)

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -9,11 +9,16 @@ function theme_enqueue_styles()
 
 
 function pd_logout_menu_link( $menu_items, $args ) {
-   if ($args->theme_location == 'Desktop Horizontal Menu') {
-      if (is_user_logged_in()) {
-         $menu_items .= '<li><a href="'. get_admin_url() .'">Admin</a></li>';
+   if ($args->theme_location == 'primary') {
+      if (is_user_logged_in()) {$menu_items .= '<li><a href="'. get_admin_url() .'">Admin</a></li>';
+        // $menu_items .= '<li><a href="'. get_admin_url() .'">Admin</a></li>';//
+
+         //resultat de la fonction get_admin_url()=http://localhost:81/Planty/wp-admin/
+
+        //$menu_items .= '<li><a href="http://localhost:81/Planty/Admin/">Admin</a></li>';
       }
    }
    return $menu_items;
 }
 add_filter( 'wp_nav_menu_items', 'pd_logout_menu_link', 10, 2 );
+
